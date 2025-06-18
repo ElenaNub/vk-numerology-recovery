ab943a0ab9aa7a4e6ac47adef3114d7dffe8d43c
Elena Nub <grosserussland@gmail.com>
2025-06-19 01:43:32 +0300

    nitial commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  server: {
-    host: true,
-    port: 5173,
-    allowedHosts: ['.lhr.life'],
-  },
+  base: './',    // вот это важно!
+  plugins: [react()],
 })
